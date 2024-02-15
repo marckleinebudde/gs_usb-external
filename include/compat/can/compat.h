1eb2369e6cae2f02a9fef18810977aebb6836eb9
Marc Kleine-Budde <mkl@pengutronix.de>
2024-02-15 08:44:38 +0100

    compat: add DECLARE_FLEX_ARRAY()
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>

--- include/compat/can/compat.h
+++ include/compat/can/compat.h
@@ -71,6 +71,43 @@
 })
 #endif
 
+#ifndef DECLARE_FLEX_ARRAY
+#ifdef __cplusplus
+/* sizeof(struct{}) is 1 in C++, not 0, can't use C version of the macro. */
+#define __DECLARE_FLEX_ARRAY(T, member)	\
+	T member[0]
+#else
+/**
+ * __DECLARE_FLEX_ARRAY() - Declare a flexible array usable in a union
+ *
+ * @TYPE: The type of each flexible array element
+ * @NAME: The name of the flexible array member
+ *
+ * In order to have a flexible array member in a union or alone in a
+ * struct, it needs to be wrapped in an anonymous struct with at least 1
+ * named member, but that member can be empty.
+ */
+#define __DECLARE_FLEX_ARRAY(TYPE, NAME)	\
+	struct { \
+		struct { } __empty_ ## NAME; \
+		TYPE NAME[]; \
+	}
+#endif
+
+/**
+ * DECLARE_FLEX_ARRAY() - Declare a flexible array usable in a union
+ *
+ * @TYPE: The type of each flexible array element
+ * @NAME: The name of the flexible array member
+ *
+ * In order to have a flexible array member in a union or alone in a
+ * struct, it needs to be wrapped in an anonymous struct with at least 1
+ * named member, but that member can be empty.
+ */
+#define DECLARE_FLEX_ARRAY(TYPE, NAME) \
+	__DECLARE_FLEX_ARRAY(TYPE, NAME)
+#endif
+
 /* don't export rx-offload symbols kernel-wide */
 #undef EXPORT_SYMBOL_GPL
 #define EXPORT_SYMBOL_GPL(_symbol)
